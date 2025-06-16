c5e439b09f6aa7c6bb665969115437d8285fb818
Jaewon Bae / 배재원 / ペゼウォン <135415380+bjw725@users.noreply.github.com>
2025-06-16 16:18:22 +0900

    Time: 263 ms (35.51%), Space: 13.6 MB (8.7%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def imageSmoother(self, img):
+        """
+        :type img: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        import math
+        
+        m, n = len(img), len(img[0])
+        result = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                total = 0
+                count = 0
+                # 3x3領域を走査
+                for di in [-1, 0, 1]:
+                    for dj in [-1, 0, 1]:
+                        ni, nj = i + di, j + dj
+                        if 0 <= ni < m and 0 <= nj < n:
+                            total += img[ni][nj]
+                            count += 1
+                result[i][j] = total // count  # 切り捨ての平均
+        return result
