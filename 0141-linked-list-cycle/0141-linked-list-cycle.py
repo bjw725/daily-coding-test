5078f2731496e1efcf44f798c88cc8c96ab14fa1
Jaewon Bae / 배재원 / ペゼウォン <135415380+bjw725@users.noreply.github.com>
2025-06-27 02:02:19 +0900

    Time: 41 ms (33.45%), Space: 19.6 MB (60.06%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasCycle(self, head):
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow = slow.next         # move 1 step
+            fast = fast.next.next    # move 2 steps
+            if slow == fast:
+                return True
+        
+        return False
